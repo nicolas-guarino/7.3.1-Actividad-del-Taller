e96e60f2b416b55cf73e536b25e9463f22d9fe35
Nicolas-Guarino <nicolasguarino03@gmail.com>
2023-08-10 18:59:11 -0300

    codigo con error

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,26 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+const nombre = document.querySelector("#nombre");
+const apellido = document.querySelector("#apellido");
+const email = document.querySelector("#email");
+const contraseña1 = document.querySelector("#password1");
+const contraseña2 = document.querySelector("#password2");
+const checkbox = document.querySelector("#terminos")
+
+
+function validarCampos() {
+    if (nombre.value === '' || apellido.value === '' || email.value === '' || contraseña1.value === '' || contraseña2.value === '' || checkbox.checked === false) {
+        showAlertError();
+    } if (contraseña1.value.length <= 6 || contraseña2.value.length <= 6) {
+        showAlertError();
+    } if (contraseña1.value !== contraseña2.value) {
+        showAlertError();
+    } if (!checkbox.checked) {
+        showAlertError();
+    }
+    else {
+        showAlertSuccess();
+    }
+}
