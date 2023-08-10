786bca8baca87560c60e45234e50c6ae687c09e7
Gian_Reynoso <107187285+reynoso-dev@users.noreply.github.com>
2023-08-10 19:12:53 -0300

    Update validacion.js solucion-dos
    xddd

--- js/validacion.js
+++ js/validacion.js
@@ -1,22 +1,23 @@
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
-
+   
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
-const nombre = document.querySelector("#nombre");
-const apellido = document.querySelector("#apellido");
-const email = document.querySelector("#email");
-const contraseña1 = document.querySelector("#password1");
-const contraseña2 = document.querySelector("#password2");
-const checkbox = document.querySelector("#terminos")
 
+function validateForm() {
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    const email = document.getElementById("email").value;
+    const password1 = document.getElementById("password1").value;
+    const password2 = document.getElementById("password2").value;
+    const terminosCheckbox = document.getElementById("terminos");
 
-function validarCampos() {
-    if ( nombre.value === '' || apellido.value === '' || email.value === '' || contraseña1.value === '' || contraseña2.value === '' || checkbox.checked === false || contraseña1.value.length <= 6 || contraseña2.value.length <= 6 || contraseña1.value !== contraseña2.value
-    ) {
+    if (!nombre || !apellido || !email || !password1 || !password2 || !terminosCheckbox.checked) {
+        showAlertError();
+    } else if (password1.length < 6 || password1 !== password2) {
         showAlertError();
     } else {
         showAlertSuccess();
